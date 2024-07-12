c3ca668ee6f2c78201c90cdad446fdc390f60877
hackerraushan <rnkp755@gmail.com>
2024-07-12 23:57:29 +0530

    Bug Fixes

--- app.js
+++ app.js
@@ -6,4 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//Routers
+import tradesRouter from "./routes/trades.routes.js";
+import bidsRouter from "./routes/bids.routes.js";
+
+app.use("/api/v1/trade", tradesRouter);
+app.use("/api/v1/bid", bidsRouter);
+
 export default app;
