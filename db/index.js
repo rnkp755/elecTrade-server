1e9450edbb198b2b2eb826104729546f4ea1e2ad
hackerraushan <rnkp755@gmail.com>
2024-07-12 17:36:41 +0530

    Initialized server

---
+++ db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectToMongo = async () => {
+      try {
+            const connectionInstance = await mongoose.connect(process.env.MONGO_URI);
+            console.log("Connected to MongoDB !!", connectionInstance.connection.host);
+      }
+      catch (error) {
+            console.log("MongoDB connection Failed", error);
+            process.exit(1);
+      }
+}
+
+export default connectToMongo;
